3a3f4e7763fb62eaed5ffd99aa33efae1e013a71
amandiumesha <136356239+amandiumesha@users.noreply.github.com>
2023-07-11 20:01:52 +0530

    Add files via upload

---
+++ 2023.07.11/Q1.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<?php
+$x=6;
+$y=4;
+echo "Sum:".($x+$y)."<br>";
+echo "Difference:".($x-$y)."<br>";
+echo "Product:".($x*$y)."<br>";
+echo "Division:".($x/$y)."<br>";
+?>
+</body>
+</html>
